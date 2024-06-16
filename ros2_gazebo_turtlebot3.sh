c4873f0575f32f51692d9a38110ce5761ab29e03
João Victor <76634741+joaovac@users.noreply.github.com>
2024-06-16 17:55:02 -0300

    Update ros2_gazebo_turtlebot3.sh

--- ros2_gazebo_turtlebot3.sh
+++ ros2_gazebo_turtlebot3.sh
@@ -15,12 +15,6 @@ sudo apt install -y software-properties-common
 sudo add-apt-repository universe -y
 
 sudo apt update && sudo apt install -y curl
-sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
-
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
-
-# Install development tools (optional)
-# Nessa parte encontrei erro na chave entao aqui esta um passo a passo para funcionar 
 
 # Remova o arquivo ros2.list que pode estar presente no local indicado:
 sudo rm /etc/apt/sources.list.d/ros2.list
@@ -31,11 +25,10 @@ curl http://repo.ros2.org/repos.key | sudo apt-key add -
 # Copie o arquivo trusted.gpg para o local apropriado e renomeie-o:
 sudo cp /etc/apt/trusted.gpg /usr/share/keyrings/ros-archive-keyring.gpg
 
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+
 # Adicione a fonte ROS2 ao arquivo ros2.list:
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
-
-# Atualize a lista de pacotes e instale o ros-dev-tools:
-sudo apt update && sudo apt install ros-dev-tools -y
 
 # Install ROS2 packages
 sudo apt update && sudo apt upgrade -y
